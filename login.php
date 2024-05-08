66e714612546c0ee35828a8379f05d7a71f54f1c
Rafliramdhani16 <rafliramdhani773@gmail.com>
2024-05-08 20:16:23 +0700

    upload tubes

---
+++ login.php
@@ -0,0 +1,54 @@
+<?php 
+session_start();
+// logika ketika kita sudah masuk login dan kita tidak double login
+if(isset($_SESSION['login']))
+{
+  header("Location: index.php");
+  exit;
+}
+
+require 'function.php';
+// ketika tombol ditekan
+if(isset($_POST['login'])){
+  $login = login($_POST);
+}
+?>
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>LOGIN</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+</head>
+<body>
+  <div class="container d-flex justify-content-center align-items-center" style="height: 100vh;">
+    <div class="card login-form" style="width: 500px; padding: 20px;">
+      <div class="card-body">
+        <h1 class="card-title text-center">LOGIN</h1>
+        <form method="POST" action="">
+          <div class="mb-3">
+            <label for="exampleInputUsername1" class="form-label">Username</label>
+            <input type="text" name="username" autofocus autocomplete="off" class="form-control" id="exampleInputUsername">
+          </div>
+          <div class="mb-3">
+            <label for="exampleInputPassword1" class="form-label">Password</label>
+            <input type="password" name="password" class="form-control" id="exampleInputPassword1">
+          </div>
+          <div class="mb-3 form-check">
+            <input type="checkbox" class="form-check-input" id="exampleCheck1">
+            <label class="form-check-label" for="exampleCheck1">Ingat</label>
+            <a href="registrasi.php" style="margin-left:20px;">Registrasi</a>
+          </div>
+          <?php if(isset($login['error'])): ?>
+            <p><?=$login['pesan']; ?></p>
+          <?php endif; ?>
+          <button type="submit" name="login" class="btn btn-primary">Masuk</button>
+          <div></div>
+        </form>
+      </div>
+    </div>
+  </div>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ registrasi.php
@@ -0,0 +1,63 @@
+<?php 
+require ('function.php');
+
+if(isset($_POST['registrasi'])){
+  if(registrasi($_POST) > 0){
+    echo "<script>
+      alert('user baru berhasil ditambahkan ');
+      document.location.href = 'login.php';
+      </script>";
+  } else {
+    echo"<script>
+    alert('user gagal ditambahkan ');
+    document.location.href = 'registrasi.php';
+    </script>";
+  }
+}
+?>
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>LOGIN</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    
+</head>
+  <body>
+    
+  <div class="container d-flex justify-content-center align-items-center" style="height: 100vh;">
+  <div class="card login-form" style="width: 500px; padding: 20px;">
+        <div class="card-body">
+            <h1 class="card-title text-center">Registrasi</h1>
+            <form action="" method="POST" enctype="multipart/form-data">
+            <input type="hidden" id="gambar" name="gambar">
+            <input type="hidden" id="id_role" name="id_role" value="2">
+            <div class="mb-3">
+    <label for="exampleInputUsername1" class="form-label">Username</label>
+    <input type="Text" name="username" autofocus autocomplete="off" class="form-control" id="exampleInputUsername" required>
+  </div>
+  <div class="mb-3">
+    <label for="exampleInputEmail1" class="form-label">Email address</label>
+    <input type="email" class="form-control" name="email" id="exampleInputEmail1" aria-describedby="emailHelp" required>
+  </div>
+  <div class="mb-3">
+    <label for="exampleInputPassword1" class="form-label">Password</label>
+    <input type="password" name="password1" class="form-control" id="exampleInputPassword1"  required>
+  </div>
+  <div class="mb-3">
+    <label for="exampleInputPassword1" class="form-label">Confirm Password</label>
+    <input type="password" name="password2" class="form-control" id="exampleInputPassword1" required>
+  </div>
+  <span>
+  <button type="submit" name="registrasi"class="btn btn-primary">Submit</button>
+  <a href="login.php" class="btn btn-primary">Masuk</a>
+  </span>
+</form>
+        </div>
+  </div>
+    </div>
+  </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+  </body>
+</html>
